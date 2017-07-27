eb7a953f0b0964ec177e05069af898f6aaf661f6
Huimee Sanchez <sanchezh95@gmail.com>
2017-07-27 15:50:58 -0700

    connected to mongodb: confirmed

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+/*
+   Mongo Terms:
+   collection = table
+   document   = row
+   field      = column
+*/
+
 var MongoClient = require('mongodb').MongoClient, assert = require('assert');
-var mongoose = require('mongoose');  // Load mongoose package
+//var mongoose = require('mongoose');  // Load mongoose package
  
 // Connection URL 
 var url = 'mongodb://localhost:27017/fakePinterest';
@@ -8,22 +15,37 @@ var url = 'mongodb://localhost:27017/fakePinterest';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
- 
-  //db.close();
+  insertDocs(db, function() {
+    db.close();
+  });
 });
 
-var fakePinterestSchema = new mongoose.Schema({ // Create schema
+var insertDocs = function(db, callback) {
+  
+  // Get document collection
+  var collection = db.collection('docs');
+
+  // Insert docs
+   collection.insert ({
+    name: 'Marc',
+    note: 'We did it'
+  }); 
+}
+
+//mongoose.connect('mongodb://localhost/fakePinterest');
+
+/* var fakePinterestSchema = new mongoose.Schema({ // Create schema
   name: String,
   note: String,
-})
+}) */
 
 // Create model based on schema
-var fakePinterest = mongoose.model('fakePinterest', 
-                                   fakePinterestSchema);
+/* var fakePinterest = mongoose.model('fakePinterest', 
+                                   fakePinterestSchema); */
 // Create in memory
-var testdb = new fakePinterest({name: 'Marc', note: 'We did it!'});
-
-testdb.save(function(err) {  					// Save to db
+//var testdb = new fakePinterest({name: 'Marc', note: 'We did it!'});
+ 
+/* testdb.save(function(err) {  				  	   // Save to db
   if (err) console.log(err);
   else console.log(testdb);
-});
+}); */
