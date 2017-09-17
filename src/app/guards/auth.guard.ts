fb966b0f356536f35a9bf648a8aac10396b72526
Huimee Sanchez <sanchezh95@gmail.com>
2017-09-16 18:32:49 -0700

    working on jwt auth service

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,1 +1,25 @@
-ECHO is on.
+/*
+Prevents unautheticated users from accessing restricted routes
+ */
+
+import { Injectable } from '@angular/core';
+
+// CanActivate: Interface that class can implement to be a guard
+// deciding if a route can be activated
+import { Router, CanActivate } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor (private router: Router) {}
+
+  canActivate() {
+    if (localStorage.getItem('currentUser')) {
+      return true;          // Logged in
+    }
+
+    // Not logged in, redirect to login page
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
--- src/services/authentification.service.ts
+++ src/services/authentification.service.ts
@@ -1,1 +1,33 @@
-ECHO is on.
+/*
+Used to log in and out of app.
+Log in: Post user credentials to API and check res for jwt. If there is
+  one, then authentication was successful & user details added to local
+  storage. Token saved & used by other services to set auth header of
+  http req.
+User remains logged in until logged out.
+ */
+
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+
+// Observables similar to Promises but can handle multi values over
+// time and can be cancelled
+import { Observable } from 'rxjs';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthentificationService {
+  public token: String;
+
+  constructor (private  http: Http) {
+
+    // Set token if saved in local storage
+    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.token = currentUser && currentUser.token;
+  }
+
+  login (username: String, password: String): Observable<boolean> {
+    return this.http.post()
+  }
+}
